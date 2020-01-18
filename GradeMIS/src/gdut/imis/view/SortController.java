2d18cd3b081084f196af472e2fac06e2f1196b5f
LYY-y <1312152518@qq.com>
2020-01-18 20:16:42 +0800

    GradeMIS200118

--- GradeMIS/src/gdut/imis/view/SortController.java
+++ GradeMIS/src/gdut/imis/view/SortController.java
@@ -1,12 +1,9 @@
 package gdut.imis.view;
 
-import gdut.imis.entity.EnglishStu;
 import gdut.imis.entity.Student;
 import gdut.imis.service.GradeService;
 import javafx.fxml.FXML;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
 public class SortController {
